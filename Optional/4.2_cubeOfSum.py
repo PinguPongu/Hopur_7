5ec3f1092a15ac04c557b4982f1114787f1e6189
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-09-05 11:06:08 +0000

    cube of sum

---
+++ Optional/4.2_cubeOfSum.py
@@ -0,0 +1,10 @@
+max_num = int(input())
+
+for i in range(max_num):
+    string_sum = 0
+
+    for char in str(i):
+        string_sum += int(char)
+
+    if (string_sum ** 3) == i:
+        print(i)
