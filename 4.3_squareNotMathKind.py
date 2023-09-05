a83633e040e7842cbc4d5e52425ef00dc512a529
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-09-05 10:22:32 +0000

    Kassi lagaður

--- 4.3_squareNotMathKind.py
+++ 4.3_squareNotMathKind.py
@@ -3,8 +3,7 @@ size = int(input())
 for i in range(size):
     for j in range(size):
         if j == 0 or j == (size - 1) or i == 0 or i == (size - 1):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
- 
-    print("\n")
+            print(" ", end=" ")
+    print()
