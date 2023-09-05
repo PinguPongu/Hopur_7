37ea739afbacadf4ee721f8ba78fe97bd6bbd853
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-09-05 11:52:13 +0000

    sinewave

--- Optional/4.3_sineWave.py
+++ Optional/4.3_sineWave.py
@@ -7,6 +7,6 @@ radians_per_line = number_of_waves * 2 * math.pi / number_of_lines
 for i in range(number_of_lines):
     r = i * radians_per_line
 
-    for j in range(int((math.sin(r) * 20)) + 20):
+    for j in range(int(math.sin(r) * 20) + 20):
         print("X", end="")
     print()
